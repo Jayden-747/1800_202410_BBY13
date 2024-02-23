bc9d3e708cbbeb12aeacf2fcff925a7626a61743
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-02-22 18:53:17 -0800

    firebase api added

---
+++ scripts/firebaseAPI-bby13.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxuXma6SNbKSr4AW9JV0YN-R0nlxR6NIU",
+  authDomain: "project-exce.firebaseapp.com",
+  projectId: "project-exce",
+  storageBucket: "project-exce.appspot.com",
+  messagingSenderId: "386653819109",
+  appId: "1:386653819109:web:1eaa070f343accd5c5840c",
+  measurementId: "G-MX9SYFY765",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
