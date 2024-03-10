4c1a487b05507a6e24bb1bc784d9b08e32757f2f
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-03-09 17:19:55 -0800

    updated setting page

--- js/modal.js
+++
@@ -1,3 +0,0 @@
-// Grabs the modal div
-// var modal = document.getElementById("username-modal");
-
--- js/settings.js
+++ js/settings.js
@@ -1,30 +1,52 @@
 const changeUserName = document.querySelector(".change-username");
 const changeUserNameIcon = changeUserName.querySelector(".dropdown-icon");
+const modal = document.querySelector(".modal-container");
 // import { getAuth } from "firebase/auth";
 const user = firebase.auth.currentUser;
 
-function rotateIcon() {
-  changeUserNameIcon.style.cssText =
-    "transform: rotate(0deg); transition: 0.2s ease-in;";
-  document.querySelector(".modal-container").style.display = "flex";
+/* 
+Makes the div expand to the desired height with an animation
+element = html element to change 
+arrow = the arrow to be rotated 
+content = the element inside the setting option
+height = the height the animation ends at 
+*/
+function open(element, arrow, content, height) {
+  element.style.cssText =
+    "height: " + height + "; transition: height 0.2s ease-in;";
+  arrow.style.cssText = "transform: rotate(0deg); transition: 0.2s ease-in;";
+  content.style.display = "flex";
 }
 
-function unRotateIcon() {
-  changeUserNameIcon.style.cssText =
-    "transform: rotate(90deg); transition: 0.2s ease-in;";
-  document.querySelector(".modal-container").style.display = "none";
+/* 
+Closes the div when clicked outside of
+element = html element to change 
+arrow = the arrow to be rotated 
+content = the element inside the setting option
+*/
+function close(element, arrow, content) {
+  element.style.cssText = "height: 45px; transition: height 0.2s ease-in;";
+  arrow.style.cssText = "transform: rotate(90deg); transition: 0.2s ease-in;";
+  content.style.display = "none";
 }
 
+/*
+Change Username dropdown animation trigger.
+*/
 document.addEventListener("mousedown", function (event) {
   if (changeUserName.contains(event.target)) {
     console.log("clicked");
-    rotateIcon();
+    open(changeUserName, changeUserNameIcon, modal, "230px");
   } else if (!changeUserName.contains(event.target)) {
-    unRotateIcon();
+    close(changeUserName, changeUserNameIcon);
     console.log("clicked outside");
   }
 });
 
+/*
+Changes the username to the given username on "Submit" button press 
+or click of the "Enter" button 
+*/
 document.getElementById("submit").addEventListener("click", function () {
   db.collection("users")
     .doc(user)
