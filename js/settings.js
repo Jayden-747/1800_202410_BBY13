061d9e0971057e9b271dce4c21bf971d977caa2a
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-03-11 15:38:39 -0700

    Change Username Functional

--- js/settings.js
+++ js/settings.js
@@ -49,10 +49,22 @@ or click of the "Enter" button
 */
 document.getElementById("submit").addEventListener("click", function () {
   db.collection("users")
-    .doc(user)
+    .doc(userID)
     .update({
       username: document.getElementById("user-input").value,
     });
   console.log("username set");
 });
 console.log(user);
+
+let uesrID;
+// Accesses User UID
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User logged in already or has just logged in.
+    console.log(user.uid);
+    userID = user.uid;
+  } else {
+    // User not logged in or has just logged out.
+  }
+});
