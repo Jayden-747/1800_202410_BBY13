8209cee8314789bf29d576fde39fe339c83759ce
Tommy Phuong  <tphuong4@my.bcit.ca>
2024-03-12 22:19:11 -0700

    Added session page to add workouts, populate database with js function, as well js function that inserts names of exercises into a dropdown menu

---
+++ js/session.js
@@ -0,0 +1,83 @@
+function dataExercise() {
+    var exerRef = db.collection("exercises");
+
+    exerRef.add({
+        name: "Sprints",
+        type: "Speed",
+        points: 4
+
+    });
+
+    exerRef.add({
+        name: "Deadlift",
+        type: "Strength",
+        points: 4
+
+    });
+
+    exerRef.add({
+        name: "Barbell Bench Press",
+        type: "Strength",
+        points: 3
+
+    });
+
+    exerRef.add({
+        name: "10 min Jog",
+        type: "Stamina",
+        points: 2
+
+    });
+
+    exerRef.add({
+        name: "20 min Jog",
+        type: "Stamina",
+        points: 3
+
+    });
+
+    exerRef.add({
+        name: "30 min Jog",
+        type: "Stamina",
+        points: 4
+
+    });
+
+    exerRef.add({
+        name: "Squats",
+        type: "Strength",
+        points: 4
+
+    });
+
+    exerRef.add({
+        name: "Sled Push",
+        type: "Speed",
+        points: 4
+
+    });
+}
+
+function populateDrop () {
+    const dropdown = document.getElementById('drop');
+    dropdown.innerHTML = '<option value="">Select an option..</option>'
+
+    db.collection('exercises').get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            //creates an option element
+            const option = document.createElement('option');
+            option.value = doc.id; // assigns doc id to determine which name to grab
+            option.textContent = doc.data().name; // assigns name to option which is what we want to display
+            dropdown.appendChild(option);
+
+        });
+
+    })
+    //for errors I guess??
+    // .catch(error => {
+    //     console.error('Error getting firestoredata: ', error);
+    // });
+}
+
+populateDrop();
