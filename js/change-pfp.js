d572ef363d09901cca9b3d5c01baaba1b65babba
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-04-01 15:16:13 -0700

    profile picture change working

--- js/change-pfp.js
+++ js/change-pfp.js
@@ -3,13 +3,22 @@ const pfp = document.querySelectorAll(".pfp-set");
 // Pfp options displayed in settings
 const pfpChoices = document.querySelectorAll(".pfp");
 
-let userNumber;
+var userNumber;
 // Accesses User UID
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     // User logged in already or has just logged in.
-    console.log(user.uid);
     userNumber = user.uid;
+    console.log(userNumber);
+    // currentPfp(userNumber);
+    db.collection("users")
+      .doc(userNumber)
+      .onSnapshot((pfpDoc) => {
+        let newPfp = pfpDoc.data().profile_picture;
+        document.querySelector(".pfp-set").src = newPfp;
+
+        console.log(document.querySelector(".pfp-set"));
+      });
   } else {
     // User not logged in or has just logged out.
   }
@@ -20,20 +29,13 @@ pfpChoices.forEach(function (choice) {
     let newPfp = choice.getAttribute("src");
     changePicture(newPfp);
     console.log(newPfp);
-    currentPfp();
   });
 });
 
-function currentPfp() {
-  db.collection("users")
-    .doc(userNumber)
-    .get()
-    .then((doc) => {
-      newPfp = doc.get("profile_picture");
-      console.log(newPfp);
-    });
+function currentPfp(userNumber) {
+  let newPfp = db.collection("users").doc(userNumber);
+  console.log(newPfp);
 }
-currentPfp();
 
 function changePicture(newPfp) {
   db.collection("users").doc(userNumber).update({ profile_picture: newPfp });
