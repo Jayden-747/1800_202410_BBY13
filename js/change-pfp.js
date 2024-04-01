61a49c472624595b83ceb9b2a7fec3f46574c762
Tommy Phuong  <tphuong4@my.bcit.ca>
2024-04-01 14:24:44 -0700

    home page Xp functionality

--- js/change-pfp.js
+++ js/change-pfp.js
@@ -10,6 +10,7 @@ firebase.auth().onAuthStateChanged((user) => {
     // User logged in already or has just logged in.
     console.log(user.uid);
     userNumber = user.uid;
+
   } else {
     // User not logged in or has just logged out.
   }
@@ -38,3 +39,41 @@ currentPfp();
 function changePicture(newPfp) {
   db.collection("users").doc(userNumber).update({ profile_picture: newPfp });
 }
+
+
+
+// Grabs stat fields from users that are logged in and updates the progress bars' width to match
+function updateBar() {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+        //go to the correct user document by referencing to the user uid
+        currentUser = db.collection("users").doc(user.uid) 
+        //get the document for current user.
+        currentUser.get()
+            .then(userDoc => {
+                //get the data fields of the user
+                //Calculates user total xp from all 3 points stats
+                let str = userDoc.data().Strength;
+                let spd = userDoc.data().Speed;
+                let sta = userDoc.data().Stamina;
+                let totalXP = str + spd +sta;
+                let totalLVL = Math.floor(totalXP / 100);
+                let totalBar = totalXP % 100;
+                let totalLVL_HTML = document.getElementById("totalLVL");
+                let totalXP_HTML = document.getElementById("levelXP");
+                let totalProgress = document.getElementById("totalBar")
+                totalLVL_HTML.innerHTML = totalLVL;
+                totalXP_HTML.innerHTML = totalBar;
+                totalProgress.style.width = `${totalBar}%`;
+
+
+            })
+          } else {
+              // No user is signed in.
+              console.log("No user is signed in");
+          }
+    });
+}
+
+updateBar();
